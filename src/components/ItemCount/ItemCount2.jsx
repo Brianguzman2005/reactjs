48a9537c59fa13b51f3ce1e0afcfc00ca04a6897
Brianguzman2005 <guzmanbrian206@gmail.com>
2025-01-23 19:21:56 -0300

    entrega final

--- src/components/ItemCount/ItemCount2.jsx
+++ src/components/ItemCount/ItemCount2.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 import Btn1 from '../Buttons/Btn1'
-const ItemCount = ({stock}) => {
-    const [contador, setContador] = useState(1)
+import { Slide, toast } from 'react-toastify'
+const ItemCount2 = ({inicial, stock , onAdd}) => {
+    const [contador, setContador] = useState(inicial)
 
     const sumar = () => {
         if(contador < stock) {
@@ -14,12 +15,25 @@ const ItemCount = ({stock}) => {
             setContador(contador - 1)
         }
     } 
-    // const onAdd = (contador) => {
-    //     console.log(contador)
-    // }
+    const agrerarAlCarrito = () => {
+        toast('Se agregaron productos al carrito', {
+            position: "bottom-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'colored',
+            transition: Slide,
+        })
+        onAdd(contador)
+    }
     return (
         <>
-            <Btn1 text="Comprar"/>
+            <button onClick={agrerarAlCarrito}>
+                <Btn1 text="Comprar"/>
+            </button>
             <div className='flex justify-center items-center bg-white p-2 rounded-full border border-light-gray gap-2 ml-3'>
                 <button className='overflow-x-hidden flex relative overflow-y-hidden rounded-full text-light-gray-text w-8 h-8 bg-blue hover:bg-light-blue justify-center items-center text-2xl font-bold pb-1' onClick={() => sumar()}>+</button>
                 {contador}
@@ -29,4 +43,4 @@ const ItemCount = ({stock}) => {
     )
 }
 
-export default ItemCount
+export default ItemCount2
